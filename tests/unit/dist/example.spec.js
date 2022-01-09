72d3bd41a775e0e5ebe7450e5c78613d43994236
RoboticWings <kliu9014@uni.sydney.edu.au>
2022-01-09 14:44:24 +0800

    。。。

--- tests/unit/dist/example.spec.js
+++ tests/unit/dist/example.spec.js
@@ -1,14 +1,11 @@
 "use strict";
 exports.__esModule = true;
-var chai_1 = require("chai");
-var test_utils_1 = require("@vue/test-utils");
-var HelloWorld_vue_1 = require("@/components/HelloWorld.vue");
-describe('HelloWorld.vue', function () {
-    it('renders props.msg when passed', function () {
-        var msg = 'new message';
-        var wrapper = test_utils_1.shallowMount(HelloWorld_vue_1["default"], {
-            props: { msg: msg }
-        });
-        chai_1.expect(wrapper.text()).to.include(msg);
-    });
-});
+/* describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    expect(wrapper.text()).to.include(msg)
+  })
+}) */
